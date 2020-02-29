b677df4541723ae263d9ad867693d277e0be28de
AugustineAykara <augustineaykara@gmail.com>
2020-02-29 13:50:57 +0530

    upload via git

--- UDP/Server.java
+++ UDP/Server.java
@@ -4,17 +4,21 @@ import java.util.*;
 
 class Server {
 	public static void main(String[] arg) throws IOException{
+		Scanner s = new Scanner(System.in);
 		DatagramSocket ds = new DatagramSocket(6060);
 		System.out.println(" Server Side");
 		
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
-		
 		String msg;
 		
 		DatagramPacket dp = new DatagramPacket(receiveData, receiveData.length);
 		ds.receive(dp);
+		
 		msg = new String(receiveData);
 		System.out.println(" Message from Client " + msg);
+		
+		int port = dp.getPort();
+		InetAddress ia = dp.getAddress();	
 	}
 }		
